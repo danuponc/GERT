f0d1c807e14142ba848ab42cce7901b8afe514b9
Danupon  Chansong <danuponc@gmail.com>
2015-12-14 12:38:03 +0700

    เพิ่ม Layout สำหรับการแสดงผลข้อมูลที่โหลดจาก php

---
+++ app/src/main/java/th/ac/tn/gert/Tab1Activity.java
@@ -0,0 +1,13 @@
+package th.ac.tn.gert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tab1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab1);
+    }
+}
